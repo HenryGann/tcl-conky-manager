4c103f59f10cee458cc22affd7a08248b5808368
Henry Gann <henry.gann@gmail.com>
2025-05-09 20:47:31 +1000

    feat: Adjusted process handling for hanging conky process. Fixed messagebox.

--- main.tcl
+++ main.tcl
@@ -11,18 +11,11 @@ set btn_counter 0
 foreach file $files {
 
     set btn_name ".configBtn_$btn_counter"
-    button $btn_name -text $file -command [list apply { {file} {
-        set status 0
-        if {[catch {exec conky -c $file} results options]} {
-            set details [dict get $options -errorcode]
-            if {[lindex $details 0] eq "CHILDSTATUS"} {
-                set status [lindex $details 2]
-            } else {
-                tk_messageBox -message "That config could not be properly loaded."
-            }
-        }
-    }} $file]
-
-    set btn_counter [expr ($btn_counter + 1)]
+    button $btn_name -text $file \
+    -command [list apply { {file} {
+            catch {exec conky -c $file &}
+            tk_messageBox -message "Launching conky config"
+        }} $file]
     pack $btn_name
+    set btn_counter [expr ($btn_counter + 1)]
 }
